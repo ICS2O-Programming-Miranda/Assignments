9cf009b5865e683bea39dafcdffa4b7a2d5b1fba
Miranda Bennett <miranda.bennett@ocsbstudent.ca>
2020-03-10 15:36:10 -0400

    added to animating images

--- AnimatingImagesMiranda/main.lua
+++ AnimatingImagesMiranda/main.lua
@@ -34,6 +34,8 @@ local function MoveSheep(event)
 	sheep.y = sheep.y - scrollSpeed
 	-- make the sheep appear
 	sheep.alpha = sheep.alpha + 0.1
+	-- make sheep spin
+	sheep.rotate = 40
 end
 
 --make it run over and over again
@@ -44,33 +46,41 @@ local chicken = display.newImageRect("Images/chicken.png", 140 ,140)
 chicken.x = 300
 chicken.y = 600
 chicken.alpha = 1
-local chickenXMoving = 2
+
 --Function:MoveChicken
 --Input: This function accepts an event listener
 --Output:none
 --Description: This function adds the scroll Speed to the x and y value 
 local function MoveChicken( event )
-	-- move the chicken, add the scroll speed to the x and y value 
-	chicken.y = chickenXMoving + chickenXMoving
+	-- move the chicken, add the scroll speed to the x value 
+	chicken.x = chicken.x + scrollSpeed
 	-- make chicken disappear 
 	chicken.alpha = chicken.alpha - 0.002
+	-- make chicken grow
+	chicken:scale(1.003, 1.003)
 end
 
 --make it run over and over again
 Runtime:addEventListener("enterFrame", MoveChicken)
 
-
-
-
-
-
-
-
-
 --display cow and set location
 local cow = display.newImageRect("Images/cow.png", 190, 190)
 cow.x = 750
 cow.y = 600
+cow.alpha = 1
 
+--Function:MoveCow
+--Input: This function accepts an event listener
+--Output:none
+--Description: This function adds the scroll Speed to the x and y value 
+local function MoveCow( event )
+	-- move the cow, add the scroll speed to the x and y value 
+	cow.x = cow.x - scrollSpeed
+	cow.y = cow.y - scrollSpeed
+	-- make cow disappear 
+	cow.alpha = cow.alpha - 0.003
+end
 
+--make it run over and over again
+Runtime:addEventListener("enterFrame", MoveCow)
 
